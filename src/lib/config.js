a2efff827c5bd635a21cb5ac40bba05c28afc50b
FngGlitcher <82155574+FngGlitcher@users.noreply.github.com>
2026-09-05 01:00:15 +0200

    Update config.js

--- src/lib/config.js
+++ src/lib/config.js
@@ -2,24 +2,88 @@ const fs = require('fs');
 const path = require('path');
 
 const ROOT = path.resolve(__dirname, '../..');
-const configPath = path.join(ROOT, 'config', 'config.json');
+const configPath = path.join(
+  ROOT,
+  'config',
+  'config.json'
+);
 
 function loadConfig() {
-  const config = JSON.parse(fs.readFileSync(configPath, 'utf8'));
-  return config;
+  return JSON.parse(
+    fs.readFileSync(
+      configPath,
+      'utf8'
+    )
+  );
+}
+
+function saveConfig(config) {
+  fs.mkdirSync(
+    path.dirname(configPath),
+    {
+      recursive: true
+    }
+  );
+
+  const tmp =
+    `${configPath}.tmp`;
+
+  fs.writeFileSync(
+    tmp,
+    JSON.stringify(
+      config,
+      null,
+      2
+    ) + '\n',
+    'utf8'
+  );
+
+  fs.renameSync(
+    tmp,
+    configPath
+  );
 }
 
 function getTargets(config) {
   const targets = [];
-  for (const [title, titleConfig] of Object.entries(config.titles || {})) {
-    if (!titleConfig.enabled) continue;
-    for (const [platform, endpoint] of Object.entries(titleConfig.endpoints || {})) {
-      if (!endpoint.enabled) continue;
-      const url = endpoint.url ||
+
+  for (const [
+    title,
+    titleConfig
+  ] of Object.entries(
+    config.titles || {}
+  )) {
+    if (!titleConfig.enabled) {
+      continue;
+    }
+
+    for (const [
+      platform,
+      endpoint
+    ] of Object.entries(
+      titleConfig.endpoints || {}
+    )) {
+      if (!endpoint.enabled) {
+        continue;
+      }
+
+      const url =
+        endpoint.url ||
         config.source.url_template
-          .replaceAll('{title}', title)
-          .replaceAll('{platform}', endpoint.platform_path || platform)
-          .replaceAll('{asset}', config.source.asset);
+          .replaceAll(
+            '{title}',
+            title
+          )
+          .replaceAll(
+            '{platform}',
+            endpoint.platform_path ||
+              platform
+          )
+          .replaceAll(
+            '{asset}',
+            config.source.asset
+          );
+
       targets.push({
         title,
         platform,
@@ -28,7 +92,13 @@ function getTargets(config) {
       });
     }
   }
+
   return targets;
 }
 
-module.exports = { ROOT, loadConfig, getTargets };
+module.exports = {
+  ROOT,
+  loadConfig,
+  saveConfig,
+  getTargets
+};
